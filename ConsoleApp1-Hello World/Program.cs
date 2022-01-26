390bdac9bdb73ca0da91d6e064c289b90499f3d9
mhsn99 <mahsuntay99@gmail.com>
2022-01-26 23:51:41 +0300

    First App

---
+++ ConsoleApp1-Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1_Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write("Please enter a name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter a surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Welcome {0} {1}", name, surname);
+            Console.ReadKey();
+        }
+    }
+}
