3c7ed91ebfef38f12f03f5670b3416a513c49b9a
mhsn99 <mahsuntay99@gmail.com>
2022-02-05 20:50:28 +0300

    challengecode

---
+++ CoderbyteChallenge1-FirstReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace CoderbyteChallenge1_FirstReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+            
+            Console.ReadKey();
+            
+        }
+        public static string FirstReverse(string str)
+        {
+
+            // code goes here
+            string temp = string.Empty;
+            for (int i = str.Length-1; i >=0 ; i--)
+            {
+                temp += str[i];
+            }
+            str = temp;
+            return str;
+
+        }
+    }
+}
