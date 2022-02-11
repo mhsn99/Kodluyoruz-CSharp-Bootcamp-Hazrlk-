9fba958fca9b2185848517a032b22ed861f8f8d2
mhsn99 <mahsuntay99@gmail.com>
2022-02-11 16:53:17 +0300

    Initial Commit

---
+++ ConsoleApp6-If_ElseIf_Ternary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6_If_ElseIf_Ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            // if elseif else
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Good Morning!");
+            else if (time <= 18)
+                Console.WriteLine("Have a nice day!");
+            else
+                Console.WriteLine("Goodnight!");
+
+            // ternary
+            string sonuc = time >= 6 && time <= 10 ? "Good Morning!" : time <= 18 ? "Have a nice day!" : "Goodnight!";
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+    }
+}
